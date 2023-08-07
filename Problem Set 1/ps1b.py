bef6396ff7102b87dc2e3c07523bfa78ec0f3ba3
Peter <97176773+PeterZhang26@users.noreply.github.com>
2023-08-07 15:20:05 +0200

    Problem Set 1b

--- Problem Set 1/ps1b.py
+++ Problem Set 1/ps1b.py
@@ -1,8 +1,9 @@
-# Problem Set 1a
+# Problem Set 1b
 
 annual_salary = float(input("Enter your annual salary: "))
 portion_saved = float(input("Enter the percent of your salary to save, as a decimal : "))
 total_cost = float(input("Enter the cost of your dream home: "))
+semi_annual_raise = float(input("Enter the semi annual raise, as a decimal: "))
 
 portion_down_payment = 0.25
 current_savings = 0
@@ -14,6 +15,8 @@ months = 0
 while current_savings < total_cost * portion_down_payment:
     current_savings += (current_savings * r)/12
     current_savings += monthly_salary * portion_saved
+    if months %6 == 0 and months > 0:
+        monthly_salary += monthly_salary * semi_annual_raise
     months += 1
 
 print(f"Number of months: {months}")
